7e83fb96e0e7719e02b6abd953f20d47b1ebef4f
Yen <r91522831@gmail.com>
2018-09-27 11:43:26 -0700

    fix conflict

--- anaEEGrasp_initialLearning.m
+++ anaEEGrasp_initialLearning.m
@@ -23,12 +23,9 @@ for i = 1:length(file_list)
     %% frame by frame
     eul = zeros(height(input{i}), 6);
     for time_id = 1:height(input{i})
-<<<<<<< HEAD
         if isempty(coord_obj{i, 1}{time_id, 1})
             continue;
         end
-=======
->>>>>>> 94c082b3b8e2bd721c262d4872b154b13a7932a6
         r_matrix = coord_obj{i, 1}{time_id, 1}{:, {'x_axis', 'y_axis', 'z_axis'}};
         tmp_eul = rotm2eul(r_matrix, 'ZYX') - eul_b4go; % ZYX -> pitch, yaw, roll
         eul(time_id, :) = [coord_obj{i, 1}{time_id, 1}{:, {'origin'}}', tmp_eul];
