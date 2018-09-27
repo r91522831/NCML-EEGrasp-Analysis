7b5d7c5cf75c2812dac5655f468455cf2921d7df
Yen <r91522831@gmail.com>
2018-09-27 10:48:04 -0700

    sep27discussion done

--- anaEEGrasp_plot_for_discussion.m
+++ anaEEGrasp_plot_for_discussion.m
@@ -1,8 +1,8 @@
 close all; clearvars; clc
 
 %% load aligned data
-[filename, pathname, ~] = uigetfile;
-load(fullfile(pathname, filename));
+[filename, pathname_plot, ~] = uigetfile;
+load(fullfile(pathname_plot, filename));
 
 %% lowpass filter all data
 % % % dt = diff(data{1}{1:2, 1}) * 0.001; % in second
@@ -17,12 +17,17 @@ timestamp_go2fini = cell(1, 1);
 obj_go2fini = cell(1, 1);
 th_go2fini = cell(1, 1);
 re_go2fini = cell(1, 1);
+in_go2fini = cell(1, 1);
+mi_go2fini = cell(1, 1);
 tmp_ind = 1;
+session = 'PT'; %'TR'; %'IL';
 for i = 1:length(file_list)
     % get only trials from the initial learing session
-    if file_list(i).name(:, 11) ~= 'I'
+    if ~strcmp(file_list(i).name(:, 11:12), session)
         continue;
     end
+    
+    obj_side = file_list(i).name(:, end - 4);
     % plot obj
     onset_t = info_time_trigger{i, 1}(ind_lft_onset(i, 2), 1); % in ms
     go2fini = ismember(info_time_trigger{i, 2}, 2:4);
@@ -30,16 +35,40 @@ for i = 1:length(file_list)
     obj_go2fini{tmp_ind, 1} = obj{i, 1}(go2fini, :);
     th_go2fini{tmp_ind, 1} = fgr_all{i, 1}.Th{:}(go2fini, :);
     re_go2fini{tmp_ind, 1} = fgr_all{i, 1}.Re{:}(go2fini, :);
+    in_go2fini{tmp_ind, 1} = fgr_all{i, 1}.In{:}(go2fini, :);
+    mi_go2fini{tmp_ind, 1} = fgr_all{i, 1}.Mi{:}(go2fini, :);
     
     tmp_ind = tmp_ind + 1;
 end
 
+ini_id = [];
+if strcmp(session, 'PT')
+    ini_id = 3;
+    timestamp_go2fini = timestamp_go2fini(ini_id:3:end, 1);
+    obj_go2fini = obj_go2fini(ini_id:3:end, 1);
+    th_go2fini = th_go2fini(ini_id:3:end, 1);
+    re_go2fini = re_go2fini(ini_id:3:end, 1);
+    in_go2fini = in_go2fini(ini_id:3:end, 1);
+    mi_go2fini = mi_go2fini(ini_id:3:end, 1);
+end
+
+
 % for plot
 colorset = flipud(gray);%parula;%cool;%varycolor(19);
 ntrial = length(timestamp_go2fini);
-color_id = floor( (1/ntrial) * 0.75 * length(colorset) );
+color_id = ceil( (1/ntrial) * 0.75 * length(colorset) );
 
-%% obj in IL
+tmp_t = zeros(ntrial, 2);
+for i = 1:ntrial
+    tmp_t(i, 1) = min(timestamp_go2fini{i});
+    tmp_t(i, 2) = max(timestamp_go2fini{i});
+end
+min_t = min(tmp_t(:, 1)) - 0.05;
+max_t = max(tmp_t(:, 2)) + 0.05;
+
+
+
+%% obj in tag
 figure(1);
 subplot 321
 hold on
@@ -48,6 +77,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('x (mm)')
 xticklabels([])
 subplot 323
@@ -57,6 +87,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('y (mm)')
 xticklabels([])
 subplot 325
@@ -75,6 +106,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('pitch ({\circ})')
 xticklabels([])
 subplot 324
@@ -84,6 +116,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('yaw ({\circ})')
 xticklabels([])
 subplot 326
@@ -93,98 +126,78 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('roll ({\circ})')
 xlabel('time (s)')
-mtit([file_list(1, 1).name(1:4), ' object pose in IL'])
-savefig(fullfile(pathname, 'plots', [file_list(1, 1).name(1:4), '_obj_pose_il']))
+mtit([file_list(1, 1).name(1:4), ' object pose in ', session, num2str(ini_id), ' ', obj_side])
+savefig(fullfile(pathname_plot, 'plots', [file_list(1, 1).name(1:4), ['_obj_pose_', session, num2str(ini_id)]]))
 
-%% thumb in IL
+%% thumb in tag
 figure(2);
-subplot 331
+subplot 321
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'fx'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fx (N)')
 xticklabels([])
-subplot 334
+subplot 323
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'fy'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fy (N)')
 xticklabels([])
-subplot 337
+subplot 325
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'fz'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fz (N)')
 xlabel('time (s)')
-subplot 332
+subplot 322
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'mx'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('mx (N-mm)')
 xticklabels([])
-subplot 335
+subplot 324
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'my'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('my (N-mm)')
 xticklabels([])
-subplot 338
+subplot 326
 hold on
 for i = 1:ntrial
     plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'mz'}, 'Color', colorset(i * color_id, :))
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('mz (N-mm)')
 xlabel('time (s)')
-subplot 333
-hold on
-for i = 1:ntrial
-    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'x'}, 'Color', colorset(i * color_id, :))
-    vline(0)
-end
-hold off
-ylabel('COPx (mm)')
-xticklabels([])
-subplot 336
-hold on
-for i = 1:ntrial
-    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'y'}, 'Color', colorset(i * color_id, :))
-    vline(0)
-end
-hold off
-ylabel('COPy (mm)')
-xticklabels([])
-subplot 339
-hold on
-for i = 1:ntrial
-    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'z'}, 'Color', colorset(i * color_id, :))
-    vline(0)
-end
-hold off
-ylabel('COPz (mm)')
-xlabel('time (s)')
-mtit([file_list(1, 1).name(1:4), ' thumb in IL'])
-savefig(fullfile(pathname, 'plots', [file_list(1, 1).name(1:4), '_th_il']))
+mtit([file_list(1, 1).name(1:4), ' thumb in ', session, num2str(ini_id), ' ', obj_side])
+savefig(fullfile(pathname_plot, 'plots', [file_list(1, 1).name(1:4), ['_th_', session, num2str(ini_id)]]))
 
-%% resultant in IL
+%% resultant in tag
 figure(3);
 subplot 321
 hold on
@@ -193,6 +206,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fx (N)')
 xticklabels([])
 subplot 323
@@ -202,6 +216,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fy (N)')
 xticklabels([])
 subplot 325
@@ -211,6 +226,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('fz (N)')
 xlabel('time (s)')
 subplot 322
@@ -220,6 +236,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('mx (N-mm)')
 xticklabels([])
 subplot 324
@@ -229,6 +246,7 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('my (N-mm)')
 xticklabels([])
 subplot 326
@@ -238,14 +256,148 @@ for i = 1:ntrial
     vline(0)
 end
 hold off
+xlim([min_t, max_t])
 ylabel('mz (N-mm)')
 xlabel('time (s)')
-mtit([file_list(1, 1).name(1:4), ' resultant in IL'])
-savefig(fullfile(pathname, 'plots', [file_list(1, 1).name(1:4), '_re_il']))
+mtit([file_list(1, 1).name(1:4), ' resultant in ', session, num2str(ini_id), ' ', obj_side])
+savefig(fullfile(pathname_plot, 'plots', [file_list(1, 1).name(1:4), ['_re_', session, num2str(ini_id)]]))
 
-%% obj in TR
-
-%% obj in PT
+%% fingers in tag
+figure(4);
+subplot 341
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'COPx'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-20, 20])
+ylabel('COPx (mm) ATI')
+xticklabels([])
+subplot 345
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'COPy'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-60, 60])
+ylabel('COPy (mm) ATI')
+xticklabels([])
+subplot 349
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'COPz'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-20, -10])
+ylabel('COPz (mm) ATI')
+xlabel('time (s)')
+subplot 342
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'x'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-20, 20])
+ylabel('COPx (mm)')
+xticklabels([])
+subplot 346
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'y'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-60, 60])
+ylabel('COPy (mm)')
+xticklabels([])
+subplot(3, 4, 10)
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, th_go2fini{i}{:, 'z'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-50, -25])
+ylabel('COPz (mm)')
+xlabel('time (s)')
+subplot 343
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, in_go2fini{i}{:, 'x'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-20, 20])
+ylabel('IN COPx (mm)')
+xticklabels([])
+subplot 347
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, in_go2fini{i}{:, 'y'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-60, 60])
+ylabel('IN COPy (mm)')
+xticklabels([])
+subplot(3, 4, 11)
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, in_go2fini{i}{:, 'z'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([25, 50])
+ylabel('IN COPz (mm)')
+xlabel('time (s)')
+subplot 344
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, mi_go2fini{i}{:, 'x'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-20, 20])
+ylabel('MI COPx (mm)')
+xticklabels([])
+subplot 348
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, mi_go2fini{i}{:, 'y'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([-60, 60])
+ylabel('MI COPy (mm)')
+xticklabels([])
+subplot(3, 4, 12)
+hold on
+for i = 1:ntrial
+    plot(timestamp_go2fini{i}, mi_go2fini{i}{:, 'z'}, 'Color', colorset(i * color_id, :))
+    vline(0)
+end
+hold off
+xlim([min_t, max_t])
+ylim([25, 50])
+ylabel('MI COPz (mm)')
+xlabel('time (s)')
+mtit([file_list(1, 1).name(1:4), ' fingers in ', session, num2str(ini_id), ' ', obj_side])
+savefig(fullfile(pathname_plot, 'plots', [file_list(1, 1).name(1:4), ['_fgr_', session, num2str(ini_id)]]))
 
 
 
