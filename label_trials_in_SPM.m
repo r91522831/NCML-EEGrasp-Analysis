bce08f67e964b28682996cb8b98660604972c890
Yen <r91522831@gmail.com>
2018-11-02 16:46:29 -0700

    Update label_trials_in_SPM.m

--- label_trials_in_SPM.m
+++ label_trials_in_SPM.m
@@ -9,10 +9,18 @@ load(fullfile(pathname, filename));
 sub_dir = uigetdir('', 'select subject folder for behvior matlab raw data.');
 file_list = dir(fullfile(sub_dir, '*.csv'));
 tmp = char({file_list.name});
+
 cond = tmp(:, [11:12, 16:18]);
+session = [tmp(:, 11:12), num2str(zeros(length(tmp), 1)), tmp(:, 13:14)];
 
 for i = 1:length(D.trials)
-    D.trials(i).label = cond(i, :);
+    if strcmp(cond(i, 1), 'I')
+        D.trials(i).label = session(i, :);
+        D.trials(i).tag = cond(i, :);
+    else
+        D.trials(i).label = cond(i, :);
+        D.trials(i).tag = session(i, :);
+    end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 save(fullfile(pathname, filename), 'D');
