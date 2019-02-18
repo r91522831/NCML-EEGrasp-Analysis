da2eff95fe92df5c463495af2aa17ac1231e632a
Yen <r91522831@gmail.com>
2019-02-18 10:46:12 -0700

    include hold event

--- EEGLab/insertEvent2EEG.m
+++ EEGLab/insertEvent2EEG.m
@@ -8,12 +8,12 @@ trial_filelist = dir(fullfile(fileparts(pathname), filename(1:4), '*.csv'));
 trial_list = cellfun(@(x) str2double(x(7:9)), {trial_filelist.name}');
 nb_trial = length(tmp_ready);
 
+% for onset time
 tmp_onset = behavior.info_onset_time .* EEG.srate;
 tmp_behavior = nan(nb_trial, 1);
 for i = 1:length(trial_list)
     tmp_behavior(trial_list(i), 1) = tmp_onset(i);
 end
-
 tmp_latency = num2cell(tmp_behavior + tmp_ready); % add 's9'(ready) time to the event time
 
 for i = 1:length(tmp_latency)
@@ -28,7 +28,21 @@ end
 tmp_onset = array2table(tmp_latency, 'VariableNames', {'latency', 'duration', 'channel', 'bvtime', 'bvmknum', 'type', 'code', 'urevent'});
 tmp_onset = table2struct(tmp_onset);
 
-tmp_event = [EEG.event, tmp_onset'];
+% for hold time
+tmp_hold = num2cell(0.5 * ([EEG.event(strcmp({EEG.event.type}, 's65')).latency] + [EEG.event(strcmp({EEG.event.type}, 's33')).latency])');
+for i = 1:length(tmp_hold)
+    tmp_hold{i, 2} = 1; % duration
+    tmp_hold{i, 3} = 0; % channel
+    tmp_hold{i, 4} = []; % bvtime
+    tmp_hold{i, 5} = []; % bvmknum
+    tmp_hold{i, 6} = 'hold'; % type
+    tmp_hold{i, 7} = 'Behavior'; % code
+    tmp_hold{i, 8} = []; % urevent
+end
+tmp_hold = array2table(tmp_hold, 'VariableNames', {'latency', 'duration', 'channel', 'bvtime', 'bvmknum', 'type', 'code', 'urevent'});
+tmp_hold = table2struct(tmp_hold);
+
+tmp_event = [EEG.event, tmp_onset', tmp_hold'];
 [~, tmp_ind] = sort([tmp_event.latency]);
 event_with_onset = tmp_event(tmp_ind);
 for i = 1:length(tmp_event)
