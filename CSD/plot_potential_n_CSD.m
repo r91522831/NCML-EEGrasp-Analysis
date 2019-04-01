bea4dacf8989f3af28ee44858be48a3546ac70f6
yen <yenhsunwu@gmail.com>
2019-04-01 15:34:54 -0700

    Update plot_potential_n_CSD.m

--- CSD/plot_potential_n_CSD.m
+++ CSD/plot_potential_n_CSD.m
@@ -26,23 +26,25 @@ for i = 1%:nb_epoch
 end
 %}
 %%
-nb_epoch = 10;
+id_epoch = [1, 2, 3, 17, 18, 19, 20, 21, 22, 24];
+nb_epoch = length(id_epoch);
 figure
 for t = 1:100:length(EEG.dataCSD(:, :, 1))
-    for i = 1:nb_epoch
+    for i = 1:length(id_epoch)
         subplot(nb_epoch, 3, 3 * (i - 1) + 1)
-        topoplot(EEG.dataCSD(:, t, i), EEG.chanlocs, 'style', 'map', 'electrodes', 'off');%, 'maplimits', 'maxmin');
+        topoplot(EEG.dataCSD(:, t, id_epoch(i)), EEG.chanlocs, 'style', 'map', 'electrodes', 'off');%, 'maplimits', 'maxmin');
         if i == 1
             title('CSD')
         end
         subplot(nb_epoch, 3, 3 * (i - 1) + 2)
-        topoplot(EEG.data(:, t, i), EEG.chanlocs, 'style', 'map', 'electrodes', 'off');%, 'maplimits', 'maxmin');
+        topoplot(EEG.data(:, t, id_epoch(i)), EEG.chanlocs, 'style', 'map', 'electrodes', 'off');%, 'maplimits', 'maxmin');
         if i == 1
             title('potential')
         end
         subplot(nb_epoch, 3, 3 * (i - 1) + 3);
-        plot(EEG.behavior.obj_epoch{i, 1}.time, EEG.behavior.obj_epoch{i, 1}.roll);
-        vline(EEG.behavior.obj_epoch{i, 1}.time(round(t * EEG.behavior.behavior_srate / EEG.srate)), '-b');
+        plot(EEG.behavior.obj_epoch{id_epoch(i), 1}.time, EEG.behavior.obj_epoch{id_epoch(i), 1}.roll);
+        ylim([-0.5, 0.5])
+        vline(EEG.behavior.obj_epoch{id_epoch(i), 1}.time(round(t * EEG.behavior.behavior_srate / EEG.srate)), '-b');
         vline(0, ':r');
         if i == nb_epoch
             xlabel('time (ms)');
@@ -50,6 +52,7 @@ for t = 1:100:length(EEG.dataCSD(:, :, 1))
             xticklabels([]);
         end
         ylabel('object roll ({\circ})');
+        
     end
     pause
     
