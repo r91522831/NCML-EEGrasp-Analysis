fc03808dced9c9b29c3f2260a618db619061bce3
yen <yenhsunwu@gmail.com>
2019-05-05 10:41:38 -0700

    apply csd

--- CSD/covertEEGLab2CSD.m
+++ CSD/covertEEGLab2CSD.m
@@ -1,72 +1,54 @@
 clear; close all; clc;
 %% Select eeg data set folder
 eeg_dataset_folder = uigetdir();
-eeg_result_folder = fullfile(fileparts(eeg_dataset_folder), 'eeg_csd');
-if ~exist(eeg_result_folder, 'dir')
-    mkdir(eeg_result_folder);
-end
-[project_root, ~, ~] = fileparts(fileparts(fileparts(fileparts(eeg_dataset_folder))));
-behavior_folder = fullfile(project_root, 'behavior', 'matlab data', 'preliminary results');
-eeg_dataset_list = dir(fullfile(eeg_dataset_folder, '*.set'));
+eeg_dataset_list = dir(fullfile(eeg_dataset_folder, 'sub-*'));
 
 %% turn on EEGLab and select eeg dataset .set file
 [ALLEEG, ~, ~, ALLCOM] = eeglab;
-% Create G and H for CSD; only need to run one time for the whole project (across subjects and trials)
-% load eeg dataset
-EEG = pop_loadset('filename', eeg_dataset_list(1).name, 'filepath', eeg_dataset_folder);
-[ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
-% get EEG montage
-E = {EEG.chanlocs.labels}'; 
-% convert ANTneuro EEG montage to CSD standarded nomenclature
-E{strcmp(E, 'M1')} = 'TP9';
-E{strcmp(E, 'M2')} = 'TP10';
-% extract montage spherical coordinates for CSD
-M = ExtractMontage('10-5-System_Mastoids_EGI129.csd', E);
-% plot electrodes
-MapMontage(M);
-% Generate Transformation Matrices G and H
-[G,H] = GetGH(M);
-% clear dataset
-STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG = []; CURRENTSET = [];
 
 %% for individual subjects
 for sub_i = 1:length(eeg_dataset_list)
-    eeg_dataset_file = eeg_dataset_list(sub_i).name;
-    %% load behavior info for all trials of one subject
-    sub_id = eeg_dataset_file(1:4);
-    % load lift onset
-    lft_onset = load(fullfile(behavior_folder, [sub_id, '_info_onset_time.mat']));
-    tmp = fieldnames(lft_onset);
-    lft_onset = lft_onset.(tmp{:}); % lift onset for each epoch in seconds
-    % load behavior
-    behavior = load(fullfile(behavior_folder, [sub_id, '_for_plot.mat']));
-    obj_behavior = cell(length(behavior.obj), 1);
-    ind_lft_onset = nan(length(behavior.obj), 1);
-    for i = 1:length(behavior.obj)
-        tmp_time = array2table(behavior.info_time_trigger{i, 1} - lft_onset(i) * 1000, 'VariableNames', {'time'});
-        tmp_time.Properties.VariableUnits = {'ms'};
-        tmp_obj = behavior.obj{i};
-        tmp_obj.Properties.VariableUnits = {'mm', 'mm', 'mm', 'deg', 'deg', 'deg'};
-        obj_behavior{i} = [tmp_time, tmp_obj];
+    tmp_dataset_folder = fullfile(eeg_dataset_folder, eeg_dataset_list(sub_i).name);
+    tmp_dataset_list = dir(fullfile(tmp_dataset_folder, '*_combine_behavior.set')); % Should be only one file
+    if ~exist(fullfile(tmp_dataset_folder, tmp_dataset_list(1).name), 'file')
+        disp('EEG data set name is not correct!')
+    end
+    
+    if sub_i == 1
+        %% Create G and H for CSD; only need to run one time for the whole project (across subjects and trials)
+        % load eeg dataset
+        EEG = pop_loadset('filename', tmp_dataset_list(1).name, 'filepath', tmp_dataset_folder);
+        [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+        % get EEG montage
+        E = {EEG.chanlocs.labels}';
+        % convert ANTneuro EEG montage to CSD standarded nomenclature
+        E{strcmp(E, 'M1')} = 'TP9';
+        E{strcmp(E, 'M2')} = 'TP10';
+        % extract montage spherical coordinates for CSD
+        M = ExtractMontage('10-5-System_Mastoids_EGI129.csd', E);
+        % plot electrodes
+        MapMontage(M);
+        % Generate Transformation Matrices G and H
+        [G,H] = GetGH(M);
+        % clear dataset
+        STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG = []; CURRENTSET = [];
     end
     
     %% load eeg dataset
-    EEG = pop_loadset('filename', eeg_dataset_file, 'filepath', eeg_dataset_folder);
+    EEG = pop_loadset('filename', tmp_dataset_list(1).name, 'filepath', tmp_dataset_folder);
     [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
-    obj_epoch = cell(size(obj_behavior));
-    for i = 1:length(obj_behavior)
-        EEG.behavior.obj_epoch{i, 1} = obj_behavior{i, 1}(obj_behavior{i, 1}.time >= EEG.times(1) & obj_behavior{i, 1}.time <= EEG.times(end), :);
-        EEG.behavior.behavior_srate = 1000 / (EEG.behavior.obj_epoch{1, 1}{2, 'time'} - EEG.behavior.obj_epoch{1, 1}{1, 'time'});
+    for i = 1:size(EEG.data, 3)
         % Prepare the Input potentials for each trial
         D = EEG.data(:, :, i); % Channels x samples of one epoch
         
         % Apply the CSD transform
-        EEG.dataCSD(:, :, i) = CSD(D, G, H);
+        EEG.dataRaw = EEG.data;
+        EEG.data(:, :, i) = CSD(D, G, H);
     end
-    
+    sub_id = tmp_dataset_list(1).name(1:6);
     EEG.setname = [sub_id, '_eeg_csd'];
     EEG = eeg_checkset( EEG );
-    EEG = pop_saveset( EEG, 'filename', [EEG.setname, '.set'], 'filepath', eeg_result_folder);
+    EEG = pop_saveset( EEG, 'filename', [EEG.setname, '.set'], 'filepath', tmp_dataset_folder);
     [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET); 
 end
 
---
+++ EEGLab/combine_behavior2EEG.m
@@ -0,0 +1,55 @@
+clear; close all; clc;
+%% Select eeg data set folder
+eeg_dataset_folder = uigetdir();
+eeg_dataset_list = dir(fullfile(eeg_dataset_folder, 'sub-*'));
+[project_root, ~, ~] = fileparts(fileparts(fileparts(eeg_dataset_folder)));
+behavior_folder = fullfile(project_root, 'behavior', 'preliminary results');
+if ~exist(behavior_folder, 'dir')
+    disp('Behavior folder is not correct!!')
+end
+
+%% turn on EEGLab and select eeg dataset .set file
+[ALLEEG, ~, ~, ALLCOM] = eeglab;
+
+%% for individual subjects
+for sub_i = 1:length(eeg_dataset_list)
+    tmp_dataset_folder = fullfile(eeg_dataset_folder, eeg_dataset_list(sub_i).name);
+    tmp_dataset_list = dir(fullfile(tmp_dataset_folder, '*_pruned_ICA.set')); % Should be only one file
+    if ~exist(fullfile(tmp_dataset_folder, tmp_dataset_list(1).name), 'file')
+        disp('EEG data set name is not correct!')
+    end
+    
+    %% load behavior info for all trials of one subject
+    sub_id = tmp_dataset_list(1).name(1:6);
+    beh_sub_id = ['S0', tmp_dataset_list(1).name(5:6)];
+    % load lift onset
+    lft_onset = load(fullfile(behavior_folder, [beh_sub_id, '_info_onset_time.mat']));
+    tmp = fieldnames(lft_onset);
+    lft_onset = lft_onset.(tmp{:}); % lift onset for each epoch in seconds
+    % load behavior
+    behavior = load(fullfile(behavior_folder, [beh_sub_id, '_for_plot.mat']));
+    obj_behavior = cell(length(behavior.obj), 1);
+    ind_lft_onset = nan(length(behavior.obj), 1);
+    for i = 1:length(behavior.obj)
+        tmp_time = array2table(behavior.info_time_trigger{i, 1} - lft_onset(i) * 1000, 'VariableNames', {'time'});
+        tmp_time.Properties.VariableUnits = {'ms'};
+        tmp_obj = behavior.obj{i};
+        tmp_obj.Properties.VariableUnits = {'mm', 'mm', 'mm', 'deg', 'deg', 'deg'};
+        obj_behavior{i} = [tmp_time, tmp_obj];
+    end
+    
+    %% load eeg dataset
+    EEG = pop_loadset('filename', tmp_dataset_list(1).name, 'filepath', tmp_dataset_folder);
+    [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+    obj_epoch = cell(size(obj_behavior));
+    for i = 1:length(obj_behavior)
+        EEG.behavior.obj_epoch{i, 1} = obj_behavior{i, 1}(obj_behavior{i, 1}.time >= EEG.times(1) & obj_behavior{i, 1}.time <= EEG.times(end), :);
+        EEG.behavior.behavior_srate = 1000 / (EEG.behavior.obj_epoch{1, 1}{2, 'time'} - EEG.behavior.obj_epoch{1, 1}{1, 'time'});
+    end
+    
+    EEG.setname = [sub_id, '_combine_behavior'];
+    EEG = eeg_checkset( EEG );
+    EEG = pop_saveset( EEG, 'filename', [EEG.setname, '.set'], 'filepath', tmp_dataset_folder);
+    [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET); 
+end
+
