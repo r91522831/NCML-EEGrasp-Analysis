ce5412862e8edbb4459a715f2e2062ebf930ffb9
yen <yenhsunwu@gmail.com>
2019-06-01 15:58:31 -0700

    Update ERSP_reg.m

--- regression/ERSP_reg.m
+++ regression/ERSP_reg.m
@@ -52,43 +52,71 @@ for All_i = selected_sub% 1:length(All_dirlist)
     electrodes_name = {'Fz', 'FCz', 'C3', 'CP3', 'C1'};
     electrodes = {Fz; FCz; C3; CP3; C1};
     nb_epoch = length(EEG.epoch);
-    tf_ersp = cell(nb_epoch, length(electrodes));
+    tf_ersp = cell(length(electrodes), 1);
     tf_itc = tf_ersp; tf_powbase = tf_ersp;
     tf_times = tf_ersp; tf_freqs = tf_ersp;
     tf_data = tf_ersp;
     
     % time frequency analysis for each channel and each epoch
     for i = 1:length(electrodes)
+        tmp_ersp = cell(nb_epoch, 1);
+        tmp_itc = tmp_ersp; tmp_powbase = tmp_ersp;
+        tmp_times = tmp_ersp; tmp_freqs = tmp_ersp;
+        tmp_data = tmp_ersp;
         for j = 1:nb_epoch
-            figure;
-            [tf_ersp{j, i}, tf_itc{j, i}, tf_powbase{j, i}, tf_times{j, i}, tf_freqs{j, i}, ~, ~, tf_data{j, i}] = ...
-                       newtimef(tfEEG.data(electrodes{i}, :, j), size(tfEEG.data, 2), [tfEEG.times(1), tfEEG.times(end)], tfEEG.srate, [3, 0.5], ...
-                                    'maxfreq', 35, ...
-                                    'topovec', 15, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', electrodes_name{i}, ...
-                                    'baseline', [-600, -100], 'basenorm', 'on', 'trialbase', 'full', 'padratio', 1, 'winsize', 512, ...
-                                    'plotitc' , 'off', 'plotphase', 'off'); % 'plotersp', 'off'
+%             figure;
+            [tmp_ersp{j, 1}, tmp_itc{j, 1}, tmp_powbase{j, 1}, tmp_times{j, 1}, tmp_freqs{j, 1}, ~, ~, tmp_data{j, 1}] = ...
+            newtimef(tfEEG.data(electrodes{i}, :, j), size(tfEEG.data, 2), [tfEEG.times(1), tfEEG.times(end)], tfEEG.srate, [3, 0.5], ...
+                                                      'maxfreq', 35, ...
+                                                      'topovec', 15, 'elocs', EEG.chanlocs, 'chaninfo', EEG.chaninfo, 'caption', electrodes_name{i}, ...
+                                                      'baseline', [-600, -100], 'basenorm', 'on', 'trialbase', 'full', 'padratio', 1, 'winsize', 512, ...
+                                                      'plotitc' , 'off', 'plotphase', 'off', 'plotersp', 'off');
         end
+        
+        % convert
+        tf_ersp{i} = cat(3, tmp_ersp{:, 1});
+        tf_itc{i} = cat(3, tmp_itc{:, 1});
+        tf_powbase{i} = cat(3, tmp_powbase{:, 1});
+        tf_times{i} = cat(3, tmp_times{:, 1});
+        tf_freqs{i} = cat(3, tmp_freqs{:, 1});
+        tf_data{i} = cat(3, tmp_data{:, 1});
     end
     
     % the continuous variables: roll angle (deg)
-    err = nan(nb_epoch, size(tf_ersp{1, 1}, 2));
+    roll_ang = nan(nb_epoch, size(tf_ersp{1, 1}, 2));
     for i = 1:nb_epoch
-        err(i, :) = spline(EEG.behavior.obj_epoch{i, 1}.time, EEG.behavior.obj_epoch{i, 1}.roll, tf_times{i, 1});
+        roll_ang(i, :) = spline(EEG.behavior.obj_epoch{i, 1}.time, EEG.behavior.obj_epoch{i, 1}.roll, squeeze(tf_times{1, 1}(:, :, 1)));
     end
     % the categorical variables: condition (IL, TR, PT)
     dummy_cond = dummyvar( grp2idx({EEG.epoch.condType}'));
-    
+    Model_Cond_IL = dummy_cond(:, 1); Model_Cond_TR = dummy_cond(:, 2); Model_Cond_PT = dummy_cond(:, 3);    
     
     % perform regression using power as depend variable; dummy conditions
     % and err as independ variables
     
-    % fit = fitlm(ft, 'ERSP~Err*Cond')
-    % E(ERSP) = beta_0 + beta_1 * Err + beta_2 * I[IL] + beta_3 * I[TR] + beta_4 * I[PT] + beta_5 * Err * I[IL] + beta_6 * Err * I[TR] + beta_7 * Err * I[PT]
+    % fit = fitlm(ft, 'ERSP ~ Roll * Cond')
+    % E(ERSP) = beta_0 * I(IL) + beta_1 * I(TR) + beta_2 * I(PT) + beta_3 * I(IL) * Roll + beta_4 * I(TR) * Roll + beta_5 * I(PT) * Roll
     % each subject results in 8 beta images in the time frequency plane
-    
-    
-    
-    
+
+    Model_Result = cell(length(electrodes), 1);
+    Model_coeff_est = Model_Result; Model_coeff_p = Model_Result; Model_Rsquared = Model_Result;
+    Model_Cond = categorical( grp2idx({EEG.epoch.condType}') );
+    for time_id = 1:size(tf_ersp{1, 1}, 2)
+        Model_Roll = roll_ang(time_id, :)';
+        for freq_id = 1:size(tf_ersp{1, 1}, 1)
+            for electrode_id = 1:length(electrodes)
+                Model_ERSP = squeeze( tf_ersp{electrode_id, 1}(freq_id, time_id, :) );
+                tbl = table(Model_Cond_IL, Model_Cond_TR, Model_Cond_PT, Model_Cond, Model_Roll, Model_ERSP);
+                mdl = fitlm(tbl, 'Model_ERSP ~ Model_Cond_IL + Model_Cond_TR + Model_Cond_PT + Model_Cond_IL:Model_Roll + Model_Cond_TR:Model_Roll + Model_Cond_PT:Model_Roll - 1', 'RobustOpts', 'on');
+%                 mdl = fitlm(tbl, 'Model_ERSP ~ Model_Roll * Model_Cond', 'RobustOpts', 'on');
+                
+                Model_Result{electrode_id, 1}{freq_id, time_id} = mdl;
+                Model_coeff_est{electrode_id, 1}(freq_id, time_id, :) = mdl.Coefficients{:, 'Estimate'};
+                Model_coeff_p{electrode_id, 1}(freq_id, time_id, :) = mdl.Coefficients{:, 'pValue'};
+                Model_Rsquared{electrode_id, 1}(freq_id, time_id, :) = mdl.Rsquared.Ordinary;
+            end
+        end
+    end
     
     
     
