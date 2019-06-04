478bdfd4aa3ad492a657679c08f33023f116aca4
yen <yenhsunwu@gmail.com>
2019-06-03 23:33:45 -0700

    Update plot_Beta_estimate.m

--- regression/plot_Beta_estimate.m
+++ regression/plot_Beta_estimate.m
@@ -35,22 +35,29 @@ for All_sub_i = selected_sub
             figure(h(electrode_i))
             if k ~= 3
                 for beta_i = 1:nb_beta
-                    
-                    subplot(2, 3, beta_i)
+                    ax = subplot(3, 2, beta_i);
                     imagesc(stat_image{k}{electrode_i, 1}(:, :, beta_i));
+                    colorbar;
+%                     colorbar('east');
                     set(gca, 'YDir', 'normal');
                     
-                    xticklabels(time_tick(xticks));
-                    xlabel('time(s)');
+%                     if beta_i == nb_beta
+                        xticklabels(time_tick(xticks));
+                        xlabel('time(s)');
+%                     else
+%                         ax.XTickLabels = [];
+%                     end
                     onset = find(diff(sign(time_stamp))) + 0.5;
                     vline(onset, '--r');
                     
                     yticklabels(freq_tick(yticks));
                     ylabel('freq(Hz)')
+%                     ylabel(['\beta_', num2str(beta_i - 1), ' freq(Hz)'])
                     title(['\beta_', num2str(beta_i - 1)])
                 end
             else
                 imagesc(stat_image{k}{electrode_i, 1}(:, :));
+                colorbar;
                 set(gca, 'YDir', 'normal');
                     
                 xticklabels(time_tick(xticks));
@@ -61,10 +68,13 @@ for All_sub_i = selected_sub
                 yticklabels(freq_tick(yticks));
                 ylabel('freq(Hz)')
             end
-            suptitle([sub_id, ' ', electrodes_name{electrode_i}, ' ', All_stat_plot{k}, ' ', ' (ERSP=\beta_0\cdotIL+\beta_1\cdotTR+\beta_2\cdotPT+\beta_3\cdotIL\cdotErr+\beta_4\cdotTR\cdotErr+\beta_5\cdotPT\cdotErr+E)'])
+            suptitle([sub_id, ' ', electrodes_name{electrode_i}, ' ', All_stat_plot{k}, ' ', ' (ERSP=\beta_0\cdotIL+\beta_1\cdotTR+\beta_2\cdotPT+\beta_3\cdotIL\cdotErr+\beta_4\cdotTR\cdotErr+\beta_5\cdotPT\cdotErr+\epsilon)'])
 
             filename_fig = [sub_id, '_', electrodes_name{electrode_i}, '_', All_stat_plot{k}, '.fig'];
-            savefig(h, fullfile(All_fig_path, filename_fig),'compact');
+            if ~exist(fullfile(All_fig_path, All_stat_plot{k}), 'dir')
+                mkdir(fullfile(All_fig_path, All_stat_plot{k}));
+            end
+            savefig(h(electrode_i), fullfile(All_fig_path, All_stat_plot{k}, filename_fig),'compact');
         end
     end
 end
