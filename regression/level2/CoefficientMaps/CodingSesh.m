aa1f87136aec72c776b24c19000712385a240ced
yen <yenhsunwu@gmail.com>
2019-07-10 20:23:26 -0700

    Update CodingSesh.m

--- regression/level2/CoefficientMaps/CodingSesh.m
+++ regression/level2/CoefficientMaps/CodingSesh.m
@@ -17,10 +17,10 @@ freqz = freqz(3:end);
 dd = dir(fullfile(All_dirpath, '*LinearModel_coeff.mat'));
 nfreq = 68; ntime = 200; ncoeff = 6; nsub = length(dd); nelectrode = 5;
 elec_dat = nan(nfreq, ntime, ncoeff, nsub, nelectrode);
-for i = 1:nsub
-    load(dd(i).name, 'Model_coeff_est');
+for s = 1:nsub
+    load(dd(s).name, 'Model_coeff_est');
     for e = 1:nelectrode
-        elec_dat(:, :, :, i, e) = Model_coeff_est{e, 1};
+        elec_dat(:, :, :, s, e) = Model_coeff_est{e, 1};
     end
 end
 rmpath(All_dirpath)
@@ -28,83 +28,107 @@ rmpath(All_dirpath)
 %% TOO MANY FREQUENCIES - remove every other
 elec_dat = elec_dat(2:2:68, :, :, :, :);
 elec_dat = elec_dat(3:end, :, :, :, :);
+nfreq = size(elec_dat, 1);
 
 %% mild smoothing, need to account for in p-value
-for i = 1:length(dd)
+for s = 1:nsub
     for e = 1:nelectrode
-        for s = 1:ncoeff
-            elec_dat(:, :, s, i, e) = imgaussfilt(elec_dat(:, :, s, i, e), 1.28); % ? = 1.28 => 2 voxel FWHM (full width at half maximum) smoothing
+        for b = 1:ncoeff
+            elec_dat(:, :, b, s, e) = imgaussfilt(elec_dat(:, :, b, s, e), 1.28); % sigma = 1.28 => 2 voxel FWHM (full width at half maximum) smoothing
         end
     end
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% FWHM = 2 * sqrt(2 * ln(2)) * ?  %
-% ? = 0.84932 => FWHM = 2         %
-% ? = 1.274 => FWHM = 3           %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FWHM = 2 * sqrt(2 * ln(2)) * sigma  %
+% sigma = 0.84932 => FWHM = 2         %
+% sigma = 1.274 => FWHM = 3           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Calucluate robust mean and sigma
+sig = nan(nfreq, ntime, ncoeff, nelectrode);
+mu = nan(nfreq, ntime, ncoeff, nelectrode);
 ticker = 1;
 h = waitbar(0, 'calculating robust means and covariances.');
+total_iter = ncoeff * nelectrode * nfreq * ntime;
 for b = 1:ncoeff
-    for r = 1:nelectrode
+    for e = 1:nelectrode
         for i = 1:nfreq
             for j = 1:ntime
-                [sig(i, j, b, r), mu(i, j, b, r), ~, ~] = robustcov(squeeze(elec_dat(i, j, b, :, r)), 'Method', 'fmcd');
+                [sig(i, j, b, e), mu(i, j, b, e)] = robustcov(squeeze(elec_dat(i, j, b, :, e)), 'Method', 'fmcd');
                 ticker = ticker + 1;
-                waitbar(ticker / 192000, h);
+                progress_precent = 100 * ticker / total_iter;
+                waitbar(ticker / total_iter, h, sprintf('calculating robust means and covariances. %2.2f %%', progress_precent));
             end
         end
     end
 end
+close(h)
 
-df = size(dd, 1) - 1;
+%% calculate p-values
+df = nsub - 1;
+robust_tstat = mu ./ (sqrt(sig ./ nsub));
 
-robust_tstat = mu ./ (sqrt(sig ./ size(dd, 1)));
-robust_tstat = mu ./ (sqrt(sig ./ 25));
-tdist2T = @(t,v) (1 - betainc(v ./ (v + t .^2), v ./ 2, 0.5));    % 2-tailed t-distribution
-for i = 1:6
-    for j = 1:5
-%        p(:, :, i, j) = tdist2T(robust_tstat(:, :, i, j), 25 - 1);
-         p(:, :, i, j) = 1 - tcdf((robust_tstat(:, :, i, j)), 25 - 1); %% change to correct dof
+p = nan(nfreq, ntime, ncoeff, nelectrode);
+for b = 1:ncoeff
+    for e = 1:nelectrode
+         p(:, :, b, e) = tcdf((robust_tstat(:, :, b, e)), df, 'upper'); %% change to correct dof ???? 
     end
 end
 
+%% ???????? what does the 25 mean? ????????
+%{
+% ????????????????????????????????????????????????????????????????????????
+nsub = 25;
+df = nsub - 1;
+robust_tstat = mu ./ (sqrt(sig ./ nsub));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Obtaining the p-value from the t stat and degree of freedom
+% with Statistics Toolbox, use the p = tcdf(x, nu, 'upper') function
+% otherwise use the lines below
+% tdist2T = @(t,v) (1 - betainc(v / (v + t ^2), v / 2, 0.5));    % 2-tailed t-distribution
+% tdist1T = @(t,v) 1 - (1 - tdist2T(t, v)) / 2;                  % 1-tailed t-distribution
+% where ‘t’ is the t-statistic and ‘v’ are the degrees-of-freedom
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for b = 1:ncoeff
+    for e = 1:nelectrode
+% % %         p(:, :, b, e) = tdist2T(robust_tstat(:, :, i, j), df);
+        p(:, :, b, e) = tcdf((robust_tstat(:, :, b, e)), df, 'upper'); %% change to correct dof
+    end
+end
+% ????????????????????????????????????????????????????????????????????????
+%}
 
 %% Perform FDR across time in all electrodes per beta
-time_idx = 35:104;
+time_idx = 35:104; % -100 ~ 600 ms? why select these time index? ?????????????????????
 p = p(:, time_idx, :, :);
+ntime = length(time_idx);
 
-% correct cluster sizes within electrode
+%% correct cluster sizes within electrode
 cluster_pval = 0.05;
 voxel_pval = 0.001;
-clustsizes = [];
-for r = 1:6
-    for e = 1:5
-        clustinfo = bwconncomp(p(:, :, r, e) < voxel_pval);
-        clustsizes = [clustsizes; cellfun(@numel, clustinfo.PixelIdxList)'];
-        
-        clust_threshold(r, e) = prctile(cellfun(@numel, clustinfo.PixelIdxList), 100 - cluster_pval * 100);
-    end
-end
 
-
-for r = 1:6
-    for e = 1:5
-        clustinfo = bwconncomp(p(:, :, r, e) < voxel_pval);
+p_indiv = nan(nfreq, ntime, ncoeff, nelectrode);
+clustsizes = cell(ncoeff, nelectrode);
+for b = 1:ncoeff
+    for e = 1:nelectrode
+        clustinfo = bwconncomp(p(:, :, b, e) < voxel_pval);
         % identify clusters to remove
-        clust_info = cellfun(@numel, clustinfo.PixelIdxList);
-        whichclusters2remove = find(clust_info < clust_threshold(r, e));
+        clustsizes{b, e} = cellfun(@numel, clustinfo.PixelIdxList);
+        clust_thresh = prctile( clustsizes{b, e}, 100 - cluster_pval * 100);
+        whichclusters2remove = find(clustsizes{b, e} < clust_thresh );
         
         % remove clusters
-        ptemp = p(:, :, r, e) < voxel_pval;
+        ptemp = p(:, :, b, e) < voxel_pval;
         for i = 1:length(whichclusters2remove)
             ptemp(clustinfo.PixelIdxList{whichclusters2remove(i)}) = 0;
         end
-        p_indiv(:, :, r, e) = ptemp;
+        p_indiv(:, :, b, e) = ptemp;
     end
 end
+
 disp('here are some example plots')
 figure
 contourf(timerstamps(time_idx), freqz, p_indiv(:, :, 1, 3) .* mu(:, time_idx, 1, 3), 'linecolor', 'none');
@@ -121,23 +145,21 @@ title('cluster corrected mean Error*IL at electrode Fcz');
 
 %% I STOPPEd HERE
 %% THIS IS A THRESHOLD OF CLUSTER SIZE USING CLUSTERS FROM ALL ELECTRODES
-all_elec_thres = round(prctile(clustsizes, 100 - cluster_pval * 100));
+all_elec_thres = round(prctile([clustsizes{:}], 100 - cluster_pval * 100));
 
 %%%%%% END USING CLUSTER CORRECTION APPROACH %%%%%%
 
 
 %% You can just correct at particualr frequency bands using FDR
 theta_pval = squeeze(mean(p(2:5, :, :, :), 1));
-
 beta_pval = squeeze(mean(p(12:17, :, :, :), 1));
-
 beta_high_pval = squeeze(mean(p(18:28, :, :, :), 1));
 error_rate = 0.1;
 for r = 1:6
     for e = 1:5
         p1 = theta_pval(:, r, e);
-            p1 = fdr_bh(p1(:)', error_rate);
-            theta_h1(:, r, e) = p1';
+        p1 = fdr_bh(p1(:)', error_rate);
+        theta_h1(:, r, e) = p1';
     end
 end
 figure;
