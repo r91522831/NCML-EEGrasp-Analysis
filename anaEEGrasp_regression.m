cfb4b2cbc330e2a243339ac7a14797d4c2fb7b9c
Yen <r91522831@gmail.com>
2020-03-23 15:40:43 -0700

    Update anaEEGrasp_regression.m

--- anaEEGrasp_regression.m
+++ anaEEGrasp_regression.m
@@ -1,9 +1,10 @@
 close all; clear; clc
 
-res = load('/Users/yenhsunw/Dropbox (ASU)/BIDS_format/NCML-EEGraspU_pilot/behavior_resultSummary.mat');
-
+% % % res = load('/Users/yenhsunw/Dropbox (ASU)/BIDS_format/NCML-EEGraspU_pilot/behavior_resultSummary.mat');
+res = load('/Users/yenhsunw/Dropbox (ASU)/BIDS_format/NCML-EEGrasp/behavior_resultSummary.mat');
 %%
-for sub_i = 1:3
+% % % for sub_i = 1:3 % sub-01, sub-02, sub-04
+for sub_i = [6, 1, 5, 8] % sub-11, sub-02, sub-09, sub-13
     tbl = res.All_info_trial.data{sub_i, 1}(:, {'dCOPy', 'dFy', 'gripF', 'pRoll'});
     mdl = fitlm(tbl);
     disp(res.All_info_trial.subID{sub_i, 1})
