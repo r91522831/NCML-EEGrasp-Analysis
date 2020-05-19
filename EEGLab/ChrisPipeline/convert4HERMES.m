4c4beb40bb7ad3b7c1399e8f4fffbc23291bb582
yen <yenhsunwu@gmail.com>
2020-05-19 09:51:08 -0700

    Update convert4HERMES.m

--- EEGLab/ChrisPipeline/convert4HERMES.m
+++ EEGLab/ChrisPipeline/convert4HERMES.m
@@ -1,10 +1,36 @@
-
 % prepare data for HERMES
+% raw data
+EEG = pop_loadset('filename','sub-09_SouceLocalized.set','filepath','/Users/yen-hsunwu/Dropbox (ASU)/BIDS_format/NCML-EEGrasp/sub-09/eeg/set/');
 e_type = cellfun(@(x) x(1), {EEG.epoch.eventcondType}');
 
 dataIL = EEG.data(:, :, strcmpi(e_type, 'IL'));
-save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', 'sub-09 SourceLocalized IL.mat'), 'dataIL');
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' IL.mat']), 'dataIL');
 dataTR = EEG.data(:, :, strcmpi(e_type, 'TR'));
-save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', 'sub-09 SourceLocalized TR.mat'), 'dataTR');
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' TR.mat']), 'dataTR');
 dataPT = EEG.data(:, :, strcmpi(e_type, 'PT'));
-save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', 'sub-09 SourceLocalized PT.mat'), 'dataPT');
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' PT.mat']), 'dataPT');
+
+%% remove eye artifacts
+EEG = pop_loadset('filename','sub-09_rmeye.set','filepath','/Users/yen-hsunwu/Dropbox (ASU)/BIDS_format/NCML-EEGrasp/sub-09/eeg/set/');
+e_type = cellfun(@(x) x(1), {EEG.epoch.eventcondType}');
+
+dataIL = EEG.data(:, :, strcmpi(e_type, 'IL'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' IL.mat']), 'dataIL');
+dataTR = EEG.data(:, :, strcmpi(e_type, 'TR'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' TR.mat']), 'dataTR');
+dataPT = EEG.data(:, :, strcmpi(e_type, 'PT'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' PT.mat']), 'dataPT');
+
+%% Only meaningful ICs
+EEG = pop_loadset('filename','sub-09_myguess.set','filepath','/Users/yen-hsunwu/Dropbox (ASU)/BIDS_format/NCML-EEGrasp/sub-09/eeg/set/');
+e_type = cellfun(@(x) x(1), {EEG.epoch.eventcondType}');
+
+dataIL = EEG.data(:, :, strcmpi(e_type, 'IL'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' IL.mat']), 'dataIL');
+dataTR = EEG.data(:, :, strcmpi(e_type, 'TR'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' TR.mat']), 'dataTR');
+dataPT = EEG.data(:, :, strcmpi(e_type, 'PT'));
+save(fullfile('/Users/yen-hsunwu/Dropbox (ASU)/HERMES/ushape', [EEG.setname, ' PT.mat']), 'dataPT');
+
+
+
