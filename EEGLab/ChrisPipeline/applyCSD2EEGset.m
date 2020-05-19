bdbe8c029b7d6d082dddbc8dbdd21380350fe7d8
yen <yenhsunwu@gmail.com>
2020-05-19 10:00:28 -0700

    add CSD to folder

--- EEGLab/ChrisPipeline/applyCSD2EEGset.m
+++ EEGLab/ChrisPipeline/applyCSD2EEGset.m
@@ -1,5 +1,4 @@
-
-
+function newEEG = applyCSD2EEGset(EEG)
 %% Create G and H for CSD
 % use CSD Toolbox
 % ===========
@@ -7,8 +6,6 @@
 % Version 1.1 (July 21, 2010)
 % http://psychophysiology.cpmc.columbia.edu/Software/CSDtoolbox/
 
-% load eeg dataset
-EEG = pop_loadset('filename', tmp_dataset_list(1).name, 'filepath', tmp_dataset_folder);
 % get EEG montage
 E = {EEG.chanlocs.labels}';
 % convert ANTneuro EEG montage to CSD standarded nomenclature
@@ -21,7 +18,7 @@ MapMontage(M);
 % Generate Transformation Matrices G and H
 [G,H] = GetGH(M);
 %% Apply CSD and save result
-for i = 1:size(EEG.data, 3)
+for i = 1:EEG.trials
     % Prepare the Input potentials for each trial
     D = EEG.data(:, :, i); % Channels x samples of one epoch
     
@@ -29,7 +26,9 @@ for i = 1:size(EEG.data, 3)
     EEG.dataRaw = EEG.data;
     EEG.data(:, :, i) = CSD(D, G, H);
 end
-sub_id = tmp_dataset_list(1).name(1:6);
+
+sub_id = EEG.setname(1:6);
 EEG.setname = [sub_id, '_eeg_csd'];
 EEG = eeg_checkset( EEG );
-EEG = pop_saveset( EEG, 'filename', [EEG.setname, '.set'], 'filepath', tmp_dataset_folder);
+newEEG = EEG;
+end
